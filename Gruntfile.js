3786a4ec9c88bd49c295a4ffe11064790ff1ba63
Edwin Shin <edwin.shin@yourmediashelf.com>
2015-04-30 10:08:39 +0800

    Copies app/data to dist directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -339,7 +339,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'data/*.*'
           ]
         }, {
           expand: true,
