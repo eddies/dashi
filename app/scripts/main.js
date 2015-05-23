fde109354ca8ff6952c719fcdb6b97904099e1d0
Edwin Shin <edwin.shin@yourmediashelf.com>
2015-05-23 10:24:55 +0800

    Allow linking to Bootstrap tabs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,6 +35,42 @@ $(function() {
   });
 });
 
+/**
+ * Allow linking to Bootstrap tabs
+ *
+ * see: http://stackoverflow.com/a/26430204/3811916
+ * see: https://github.com/twbs/bootstrap/issues/2415
+ */
+$(function() {
+    // cache the id
+    var navbox = $('.nav-pills');
+
+    // activate tab on click
+    // navbox.on('click', 'a', function (e) {
+    //   var $this = $(this);
+    //   // prevent the Default behavior
+    //   e.preventDefault();
+    //   // send the hash to the address bar
+    //   window.location.hash = $this.attr('href');
+    //   // activate the clicked tab
+    //   $this.tab('show');
+    // });
+
+    // will show tab based on hash
+    function refreshHash() {
+      navbox.find('a[href="' + window.location.hash + '"]').tab('show');
+    }
+
+    // show tab if hash changes in address bar
+    $(window).bind('hashchange', refreshHash);
+
+    // read hash from address bar and show it
+    if(window.location.hash) {
+      // show tab on load
+      refreshHash();
+    }
+});
+
 // Monkey patch to override behavior of turn(On|Off)Controls,
 // allowing reset link to be located in a sibling div of the chart
 dc.baseMixin = (function(original) {
